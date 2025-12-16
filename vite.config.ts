48ff867911c9c3607e18149ed3a7302c432b80da
Yuki-Onozawa <yonozawa@task-do.co.jp>
2025-12-16 11:34:18 +0900

    タイトル直接変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/gas-expense-app/',
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        data: {
+          title: '家計簿アプリ'
+        }
+      }
+    })
+  ],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+});
